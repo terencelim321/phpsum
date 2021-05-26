71cbc1787f1ac74b49d0b16985e2ec16b35a2afa
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-26 19:57:46 +0800

    Update sum.php

--- sum.php
+++ sum.php
@@ -9,14 +9,15 @@ Number 2 is: <?php echo $_POST["num2"]; ?>
 $num1 = $_POST["num1"];
 $num2 = $_POST["num2"];
 #$num1 = 2;
-#$num2 = 3;
+#$num2 = 4;
 
-$age = array("number1"=>$num1, "number2"=>$num2);
+#$age = array("number1"=>$num1, "number2"=>$num2);
 
-echo json_encode($age);
-$json = json_encode($age);
+#echo json_encode($age);
+#$json = json_encode($age);
 
-$result = shell_exec("python sum.py $json");
+#$result = shell_exec("python sum.py $json");
+$result = shell_exec("python sum.py $num1 $num2");
 echo $result;
 ?>
 
