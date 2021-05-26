85b61d5af9d18b12264bca234f1da312711c47bc
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-26 12:51:26 +0800

    Update sum.php

--- sum.php
+++ sum.php
@@ -2,8 +2,17 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Number 1 <?php echo $_POST["num1"]; ?><br>
+Number 2 is: <?php echo $_POST["num2"]; ?>
+
+<?php
+$num1 = $_POST["num1"];
+$num2 = $_POST["num2"];
+
+$output = shell_exec("python sum.py $num1 $num2");
+echo "sum of both numbers = " $output;
+?>
+
 
 </body>
 </html>
