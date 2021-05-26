4a7460c19718cba15e0a9df9bde9b6892397e92d
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-26 16:11:15 +0800

    Update sum.php

--- sum.php
+++ sum.php
@@ -11,6 +11,14 @@ $num2 = $_POST["num2"];
 
 $output = shell_exec("python sum.py $num1 $num2");
 #echo "sum of both numbers = " $output;
+
+$myObj->name = "John";
+$myObj->age = 30;
+$myObj->city = "New York";
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
 ?>
 
 
