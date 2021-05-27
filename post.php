4f4783976b93f30c6bf8b4f4ef8326a315526bc3
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-27 16:19:55 +0800

    Update post.php

--- post.php
+++ post.php
@@ -4,12 +4,12 @@ $numbers = array(
                     'number2' => 2
 
 );
-$str = http_build_query($numbers);
+#$str = http_build_query($numbers);
 
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, "https://phpsum.herokuapp.com/store.php");
 curl_setopt($curl, CURLOPT_POST,1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $str);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $numbers);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $output = curl_exec($curl);
 curl_close($curl);
