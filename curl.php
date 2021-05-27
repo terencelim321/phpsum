77eed3cbfdea66d558759b7104381049c26fe6c4
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-27 10:42:16 +0800

    Update curl.php

--- curl.php
+++ curl.php
@@ -4,12 +4,12 @@ $curl = curl_init(); //$ data type curl resource
 $url = 'https://phpsum.herokuapp.com/curl.json';
 #set curl options
 curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true)
 
 #run curl (execute http request)
 $data = curl_exec($curl);
-$data = json_decode(file_get_contents('php://input'), true);
-print_r($data);
-echo $data;
+$decoded = json_decode($data);
+print_r($decoded);
 #$a = json_decode(file_get_contents($data), true);
 #echo $a;
 #close curl resource
