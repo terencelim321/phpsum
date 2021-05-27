6d69d77a1b99f794ee6b6218891aee56a2b376aa
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-27 09:48:47 +0800

    Update sum.php

--- sum.php
+++ sum.php
@@ -17,10 +17,10 @@ $num2 = $_POST["num2"];
 #$json = json_encode($age);
 
 #$result = shell_exec("python sum.py $json");
-$sum = $num1 + $num2 ;
+#$sum = $num1 + $num2 ;
 $result = shell_exec("python sum.py $num1 $num2");
 echo $result;
-echo $sum;
+#echo $sum;
 ?>
 
 
