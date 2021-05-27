365dc980a3704bc33ac52d88cec2d7c41e2ecff2
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-27 11:00:54 +0800

    Update curl.php

--- curl.php
+++ curl.php
@@ -10,7 +10,7 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($curl);
 $decoded = json_decode($data, true);
 print_r($decoded);
-echo $data[0:10];
+echo $data[2];
 
 curl_close($curl);
 ?>
