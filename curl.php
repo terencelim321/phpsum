cea653d11b76878d2e887ff3cc491cc6d5cde708
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-27 10:55:14 +0800

    Update curl.php

--- curl.php
+++ curl.php
@@ -4,14 +4,12 @@ $curl = curl_init(); //$ data type curl resource
 $url = 'https://reqres.in/api/users?page=2';
 #set curl options
 curl_setopt($curl, CURLOPT_URL, $url);
-#curl_setopt($curl, CURLOPT_RETURNTRANSFER, true)
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 #run curl (execute http request)
 $data = curl_exec($curl);
 $decoded = json_decode($data, true);
 print_r($decoded);
-#$a = json_decode(file_get_contents($data), true);
-#echo $a;
-#close curl resource
+
 curl_close($curl);
 ?>
